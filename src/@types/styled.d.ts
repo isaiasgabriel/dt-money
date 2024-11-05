29d5724287eff6fd793cbefab0b7b0fa9c9858f9
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-05 14:35:30 -0300

    style: global style, default theme, external font

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
+
+// This configuration enables the editor to see and use the theme properties (e.g., colors) directly while coding.
+// For example, we can easily access the available theme variables with auto-suggestions without needing to open the defaultTheme file.
