13d8a1360df00f446f299827aeb8b3ece0a01c11
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-06 15:21:44 -0300

    component: modal dialog with Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog' // Docs: https://www.radix-ui.com/primitives/docs/components/dialog
 
 export function Header() {
   return (
@@ -7,7 +8,26 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>New Transaction</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            {/* 
+                The asChild option will reuse the button inside as the trigger instead of creating a new button.
+                If you don't put the 'asChild' option you'll have 2 buttons: Datalog.Trigger and NewTransactionButton.
+            */}
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.Trigger>
+
+          {/* Dialog.Portal will create the component outside the root */}
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            {/* Dialog.Overlay it's basically the background with less opacity */}
+            <Dialog.Content>
+              <Dialog.Title>New Transaction</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
