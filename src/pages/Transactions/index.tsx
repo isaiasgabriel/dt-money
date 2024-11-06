e00b73f24e54609c9e829678007f8a83d32d64a7
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-06 20:37:49 -0300

    feat: connecting to the api

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './SearchForm'
@@ -7,7 +8,34 @@ import {
   TransactionTable,
 } from './styles'
 
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  createdAt: string
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    // 1. API call
+    const response = await fetch('http://localhost:3333/transactions')
+    // 2. Convert the data into JSON
+    const data = await response.json()
+
+    // 3. set the transactions state with the data collected
+    setTransactions(data)
+  }
+
+  // We separated the useEffect and the loadTransactions functions
+  // because you can't transform the useEffect into an async function directly.
+  // Example: useEffect(async()=>{},[])
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -17,22 +45,20 @@ export function Transactions() {
         <SearchForm />
         <TransactionTable>
           <tbody>
-            <tr>
-              <td width="50%">Site development</td>
-              <td>
-                <PriceHighlight variant="income">+ R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Sell</td>
-              <td>05/11/2024</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">- R$ 50,00</PriceHighlight>
-              </td>
-              <td>Food</td>
-              <td>11/11/2024</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.type}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionTable>
       </TransactionsContainer>
