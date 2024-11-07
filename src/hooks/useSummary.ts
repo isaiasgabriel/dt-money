2bfe4d797ea98feed7e6eabc4bee22777980e63f
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-07 16:40:12 -0300

    feat: separating the context from the component
    We've basically separated out all the code responsible for taking the transactions from the context and then iterating over this data using the reduce function. All this code has gone into a hook file, so inside the Summary component we only need to execute the useSummary function to have access to the summary.

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { TransactionsContext } from '../contexts/TransactionContext'
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  // reduce is a function that iterates thorugh an array and REDUCE into a new data structure
+  // this new data structure is specified as the second argument of the function ({income: 0, outcome: 0, total: 0})
+  // acc (accumulator) represents this data structure and allow us to manipulate it's data
+  // in the end of the function we return this acc(accumulator) which now can be used in the component as summary
+  // acc === summary
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome -= transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
+  return summary
+}
