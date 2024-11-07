7f71ad97d234dd9e2c0ee00c463706e8562c70ec
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-07 16:12:08 -0300

    feat: calculating summary transactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,35 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './styles'
+import { TransactionsContext } from '../../contexts/TransactionContext'
+import { useContext } from 'react'
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  // reduce is a function that iterates thorugh an array and REDUCE into a new data structure
+  // this new data structure is specified as the second argument of the function ({income: 0, outcome: 0, total: 0})
+  // acc (accumulator) represents this data structure and allow us to manipulate it's data
+  // in the end of the function we return this acc(accumulator) which now can be used in the component as summary
+  // acc === summary
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome -= transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,15 +38,15 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
         <header>
-          <span>Expenses</span>
+          <span>Outcome</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -26,7 +54,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
