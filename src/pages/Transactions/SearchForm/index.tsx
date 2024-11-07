1cc9825fa7bbc2140d6c59138c3cd89bd7056ea3
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-07 17:35:38 -0300

    feat: search form validation with zod and hook form

--- src/pages/Transactions/SearchForm/index.tsx
+++ src/pages/Transactions/SearchForm/index.tsx
@@ -1,12 +1,46 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// 1. Define a zod schema to validate the form data
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+// 2. Define a TypeScript type based on the Zod schema.
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Search transaction" />
+  // - `register` is used to connect input fields with react-hook-form.
+  // - `handleSubmit` indicates which function to execute when the form is submitted - it must be added inside the form parent component.
+  // - `formState` gives access to the form's current state, including `isSubmitting`.
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    // By the default the react hook form doesn't have validation functions in this case we used zod
+    resolver: zodResolver(searchFormSchema),
+  })
 
-      <button>
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    // We're simulating a delay of 2 seconds to show how the buttons works when it's state is `isSubmitting`
+    // While this delay occurs, `isSubmitting` is set to true, disabling the submit button.
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Search transaction"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Search
       </button>
