2565c514610cfa470b31a3a90a51694219791573
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-17 21:34:13 -0300

    feat: modal dialog controlled components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,25 +8,30 @@ import {
 } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  // type: z.enum(['income', 'outcome']),
+  type: z.enum(['income', 'outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
   const {
+    // Each time you want to add a new info into your form you'll need to use the control from the use from
+    control,
     register,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: 'income',
+    },
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
@@ -65,19 +70,42 @@ export function NewTransactionModal() {
             {...register('category')}
           />
 
-          {/* TransactionType = RadioGroup.Root */}
-          <TransactionType>
-            {/* TransactionTypeButton = RadioGroup.Item */}
-            <TransactionTypeButton variant="income" value="income">
-              {/* 'value' is obligatory for the radio group */}
-              <ArrowCircleUp size={24} />
-              Income
-            </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
-              Outcome
-            </TransactionTypeButton>
-          </TransactionType>
+          {/* 
+            Controller - A component from the react-hook-form library used to connect custom components 
+            to the form's state and validation logic. It ensures the custom component's value is managed 
+            within the form context.
+
+            Render - A required prop for Controller. It's a function that returns the custom component 
+            you want to control. React-hook-form provides various helper properties (like `field`) to 
+            handle the field's value, changes, and other events.
+
+            ({ field }) - Destructures the `field` object provided by react-hook-form. This object contains:
+              - `field.value`: The current value of the form field ('income' or 'outcome' here).
+              - `field.onChange`: A function to update the field value when it changes.
+              - `field.onBlur`: A function called when the field loses focus.
+              - `field.name`: The name of the field being controlled ('type' in this case).
+        */}
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Income
+                  </TransactionTypeButton>
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Outcome
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
 
           <button type="submit" disabled={isSubmitting}>
             Add
